acb6bcc3c5c277d89a685dee553dc7f9de3fb7b8
ej770 <95518303+ej770@users.noreply.github.com>
2026-01-03 22:37:59 -0600

    Remove unnecessary comments in metrics_calculation.py

--- scripts/metrics_calculation.py
+++ scripts/metrics_calculation.py
@@ -11,10 +11,10 @@ os.makedirs("outputs", exist_ok=True)
 # Load data
 df = pd.read_csv(RAW_FILE)
 
-# Use year, month, and retail_sales columns to calculate KPIs
+
 kpis = df.groupby(["year", "month"])["retail_sales"].sum().reset_index()
 kpis.rename(columns={"retail_sales": "Total_Retail_Sales"}, inplace=True)
 
-# Save KPI summary
+
 kpis.to_csv(OUTPUT_KPI, index=False)
 print(f"KPI summary saved to {OUTPUT_KPI}")
